c3f6669f2238ef05aead68f4a4d8f1e522183645
Léo <leo@leogillet.com>
2023-02-07 16:49:25 +0100

    init

---
+++ assets/font.js
@@ -0,0 +1,10 @@
+var fonts = ['TerminessTTF Nerd Font', 'Space Mono', 'Victor Mono', 'Cascadia Code', 'Fantasque Sans Mono', 'Iosevka Curly', 'JetBrains Mono', 'Monofoki'];
+var font = fonts[Math.floor(Math.random() * fonts.length)];
+
+var textEls = document.getElementsByClassName('random-font');
+
+textEls.forEach(element => {
+    element.style.fontFamily = font+", monospace";
+    console.info("Font randomly chosen: "+font);
+});
+
---
+++ assets/typeit.js
@@ -0,0 +1,53 @@
+var cursorChars = ["_", "|", "&block;"];
+var cursor = cursorChars[Math.floor(Math.random() * cursorChars.length)];
+
+const jobInstance = new TypeIt("#job", {
+  cursorChar: cursor,
+  speed: 60,
+})
+  .type("bioinformatician & full-stck developer", { delay: 1000 })
+  .move(-12, { delay: 250 })
+  .type("a", { delay: 300 })
+  .options({ speed: 33 })
+  .move(null, { to: "end" })
+  .go();
+
+const ghInstance = new TypeIt("#gh", {
+  cursorChar: cursor,
+  speed: 75,
+  afterComplete: function (instance) {
+    instance.destroy();
+  },
+})
+  .type("check my github <span class='fa-brands'></span>")
+  .go();
+
+const linkedinInstance = new TypeIt("#linkedin", {
+  cursorChar: cursor,
+  speed: 75,
+  afterComplete: function (instance) {
+    instance.destroy();
+  },
+})
+  .type("find me on my linked<span class='fa-brands'></span>")
+  .go();
+
+const emailInstance = new TypeIt("#email", {
+  cursorChar: cursor,
+  speed: 75,
+  afterComplete: function (instance) {
+    instance.destroy();
+  },
+})
+  .type("send an email to <span style='font-weight: bold;'>leo@leogillet.com</span>")
+  .go();
+
+const aotyInstance = new TypeIt("#aoty", {
+  cursorChar: cursor,
+  speed: 75,
+  afterComplete: function (instance) {
+    instance.destroy();
+  },
+})
+  .type("what i'm listening to <span class='fa-solid'></span>")
+  .go();
